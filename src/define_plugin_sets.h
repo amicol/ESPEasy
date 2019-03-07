e80a3f92d9aa997a8a73979f97019e5f34a82d48
amicol <micolalexandre@gmail.com>
2019-03-07 19:21:39 +0100

    Init Sonoff 3WV

--- src/define_plugin_sets.h
+++ src/define_plugin_sets.h
@@ -153,6 +153,7 @@ To create/register a plugin, you have to :
 
     #define PLUGIN_SET_ONLY_SWITCH
     #define PLUGIN_SET_ONLY_TEMP_HUM
+    #define USES_P021
 #endif
 
 #ifdef PLUGIN_SET_SONOFF_POW
@@ -174,6 +175,9 @@ To create/register a plugin, you have to :
 #ifdef PLUGIN_SET_SONOFF_4CH
     #define PLUGIN_DESCR  "Sonoff 4CH"
     #define PLUGIN_SET_ONLY_SWITCH
+    //#define PLUGIN_SET_ONLY_TEMP_HUM
+    //#define USES_P021
+    //#define USES_P220
 #endif
 
 #ifdef PLUGIN_SET_SONOFF_TOUCH
